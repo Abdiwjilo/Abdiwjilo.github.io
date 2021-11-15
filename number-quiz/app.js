0c50f1e651211a6d6a7db961a6fe6951b72f6d81
Abdiwjilo <abdiwjilo@gmail.com>
2021-11-15 00:08:13 -0600

    Create number quiz

--- number-quiz/app.js
+++ number-quiz/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 2000;
+const port = 8080;
 const urlencodedParser = express.urlencoded({ extended: false })
 app.use(express.static('./'));
 
